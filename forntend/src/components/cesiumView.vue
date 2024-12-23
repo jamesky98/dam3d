245fc080d467876585e0bda26217bce6735b2f76
jamesky98 <jamesky98@gmail.com>
2024-12-23 23:22:26 +0800

    1131223

--- forntend/src/components/cesiumView.vue
+++ forntend/src/components/cesiumView.vue
@@ -6,6 +6,8 @@
   } from "vue";
 
   import { 
+    defined,
+    Color,
     Camera,
     Rectangle,
     Cartesian3, 
@@ -29,6 +31,12 @@
     ImageryLayer,
     defaultValue,
     buildModuleUrl,
+    CesiumTerrainProvider,
+    IonImageryProvider,
+    ImageryProvider,
+    Ellipsoid,
+    IonResource,
+    EllipsoidTerrainProvider,
   } from 'cesium';
   import "cesium/Build/Cesium/Widgets/widgets.css";
 
@@ -65,12 +73,19 @@
 
   const viewModel = {
     layers: [],
+    imglayers: [],
     baseLayers: [],
+    tilelayers: [],
+    terrainLayers: [],
     upLayer: null,
     downLayer: null,
     selectedLayer: null,
+    terrainLayer: null,
     isSelectableLayer: function (layer) {
       return this.baseLayers.indexOf(layer) >= 0;
+    },
+    isTilesleLayer: function (layer) {
+      return this.tilelayers.indexOf(layer) >= 0;
     },
     raise: function (layer, index) {
       imageryLayers.raise(layer);
@@ -92,14 +107,15 @@
       }, 10);
     },
     canRaise: function (layerIndex) {
-      return layerIndex > 0;
+      return layerIndex > this.tilelayers.length;
     },
     canLower: function (layerIndex) {
-      return layerIndex >= 0 && layerIndex < imageryLayers.length - 1;
+      return layerIndex >= this.tilelayers.length && layerIndex < (this.tilelayers.length+imageryLayers.length) - 1;
     },
   };
 
   const baseLayers = viewModel.baseLayers;
+  const terrainLayers = viewModel.terrainLayers;
   knockout.track(viewModel);
 
   function setupLayers() {
@@ -107,47 +123,47 @@
     // These base layers aren't really special.  It's possible to have multiple of them
     // enabled at once, just like the other layers, but it doesn't make much sense because
     // all of these layers cover the entire globe and are opaque.
-    addBaseLayerOption("Bing Maps Aerial", createWorldImageryAsync());
-    addBaseLayerOption(
-      "Bing Maps Road",
-      createWorldImageryAsync({
-        style: IonWorldImageryStyle.ROAD,
-      }),
-    );
-    addBaseLayerOption(
-      "ArcGIS World Street Maps",
-      ArcGisMapServerImageryProvider.fromUrl(
-        "https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer",
-      ),
-    );
+    // addBaseLayerOption("Bing Maps Aerial", createWorldImageryAsync());
+    // addBaseLayerOption(
+    //   "Bing Maps Road",
+    //   createWorldImageryAsync({
+    //     style: IonWorldImageryStyle.ROAD,
+    //   }),
+    // );
+    // addBaseLayerOption(
+    //   "ArcGIS World Street Maps",
+    //   ArcGisMapServerImageryProvider.fromUrl(
+    //     "https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer",
+    //   ),
+    // );
     addBaseLayerOption("OpenStreetMaps", new OpenStreetMapImageryProvider());
-    addBaseLayerOption(
-      "Stamen Maps",
-      new OpenStreetMapImageryProvider({
-        url: "https://stamen-tiles.a.ssl.fastly.net/watercolor/",
-        fileExtension: "jpg",
-        credit:
-          "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.",
-      }),
-    );
-    addBaseLayerOption(
-      "Natural Earth II (local)",
-      TileMapServiceImageryProvider.fromUrl(
-        buildModuleUrl("Assets/Textures/NaturalEarthII"),
-      ),
-    );
-    addBaseLayerOption(
-      "USGS Shaded Relief (via WMTS)",
-      new WebMapTileServiceImageryProvider({
-        url: "https://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer/WMTS",
-        layer: "USGSShadedReliefOnly",
-        style: "default",
-        format: "image/jpeg",
-        tileMatrixSetID: "default028mm",
-        maximumLevel: 19,
-        credit: "U. S. Geological Survey",
-      }),
-    );
+    // addBaseLayerOption(
+    //   "Stamen Maps",
+    //   new OpenStreetMapImageryProvider({
+    //     url: "https://stamen-tiles.a.ssl.fastly.net/watercolor/",
+    //     fileExtension: "jpg",
+    //     credit:
+    //       "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.",
+    //   }),
+    // );
+    // addBaseLayerOption(
+    //   "Natural Earth II (local)",
+    //   TileMapServiceImageryProvider.fromUrl(
+    //     buildModuleUrl("Assets/Textures/NaturalEarthII"),
+    //   ),
+    // );
+    // addBaseLayerOption(
+    //   "USGS Shaded Relief (via WMTS)",
+    //   new WebMapTileServiceImageryProvider({
+    //     url: "https://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer/WMTS",
+    //     layer: "USGSShadedReliefOnly",
+    //     style: "default",
+    //     format: "image/jpeg",
+    //     tileMatrixSetID: "default028mm",
+    //     maximumLevel: 19,
+    //     credit: "U. S. Geological Survey",
+    //   }),
+    // );
 
     addBaseLayerOption(
       "臺灣通用電子地圖(正射影像)",
@@ -163,18 +179,18 @@
     );
 
     // Create the additional layers
-    addAdditionalLayerOption(
-      "United States GOES Infrared",
-      new WebMapServiceImageryProvider({
-        url: "https://mesonet.agron.iastate.edu/cgi-bin/wms/goes/conus_ir.cgi?",
-        layers: "goes_conus_ir",
-        credit: "Infrared data courtesy Iowa Environmental Mesonet",
-        parameters: {
-          transparent: "true",
-          format: "image/png",
-        },
-      }),
-    );
+    // addAdditionalLayerOption(
+    //   "United States GOES Infrared",
+    //   new WebMapServiceImageryProvider({
+    //     url: "https://mesonet.agron.iastate.edu/cgi-bin/wms/goes/conus_ir.cgi?",
+    //     layers: "goes_conus_ir",
+    //     credit: "Infrared data courtesy Iowa Environmental Mesonet",
+    //     parameters: {
+    //       transparent: "true",
+    //       format: "image/png",
+    //     },
+    //   }),
+    // );
     addAdditionalLayerOption(
       "United States Weather Radar",
       new WebMapServiceImageryProvider({
@@ -199,7 +215,6 @@
   async function addBaseLayerOption(name, imageryProviderPromise) {
     try {
       const imageryProvider = await Promise.resolve(imageryProviderPromise);
-
       const layer = new ImageryLayer(imageryProvider);
       layer.name = name;
       baseLayers.push(layer);
@@ -207,6 +222,12 @@
     } catch (error) {
       console.error(`There was an error while creating ${name}. ${error}`);
     }
+  }
+  async function addTerrainLayerOption(name, terrainProviderPromise) {
+      const layer = terrainProviderPromise
+      layer.name = name;
+      terrainLayers.push(layer);
+      // updateLayerList();
   }
 
   async function addAdditionalLayerOption(
@@ -228,13 +249,58 @@
       console.error(`There was an error while creating ${name}. ${error}`);
     }
   }
+  async function add3DTilesLayerOption(
+    name,
+    asset_id,
+    alpha,
+    show,
+  ) {
+    try {
+      const layer = await Cesium3DTileset.fromIonAssetId(asset_id);
+        let options = defaultValue(null, defaultValue.EMPTY_OBJECT);
+
+        let style = options.style;
+
+        if (!defined(style)) {
+          const color = defaultValue(
+            options.defaultColor,
+            Color.WHITE,
+          ).toCssColorString();
+          style = new Cesium3DTileStyle({
+            color: `Boolean(\${feature['cesium#color']}) ? color(\${feature['cesium#color']}) : ${color}`,
+          });
+        }
+      layer.style = style;
+      layer.alpha = defaultValue(alpha, 1);
+      layer.show = defaultValue(show, true);
+      layer.name = name;
+      cs_viewer.scene.primitives.add(layer);
+
+      knockout.track(layer, ["alpha", "show", "name"]);
+      updateLayerList();
+    } catch (error) {
+      console.error(`There was an error while creating ${name}. ${error}`);
+    }
+  }
 
   function updateLayerList() {
     const numLayers = imageryLayers.length;
-    viewModel.layers.splice(0, viewModel.layers.length);
+    viewModel.imglayers.splice(0, viewModel.imglayers.length);
     for (let i = numLayers - 1; i >= 0; --i) {
-      viewModel.layers.push(imageryLayers.get(i));
+      viewModel.imglayers.push(imageryLayers.get(i));
     }
+
+    const numTileLayers = cs_viewer.scene.primitives.length;
+    viewModel.tilelayers.splice(0, viewModel.tilelayers.length);
+    for (let i = numTileLayers - 1; i >= 0; --i) {
+      viewModel.tilelayers.push(cs_viewer.scene.primitives.get(i));
+    }
+
+
+    viewModel.layers.splice(0, viewModel.layers.length);
+    viewModel.layers=[...viewModel.tilelayers, ...viewModel.imglayers];
+
+    console.log('viewModel',viewModel);
   }
 
 // Cesium初始化
@@ -242,13 +308,13 @@
     // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
     viewer = new Viewer('cesiumContainer', {
       animation: true,
-      baseLayerPicker: false,
+      baseLayerPicker: true,
       // fullscreenButton: false,
       // vrButton: false,
       geocoder: false,
-      // homeButton: false,
+      homeButton: true,
       // infoBox: false,
-      sceneModePicker: false,
+      sceneModePicker: true,
       // selectionIndicator: false,
       timeline: true,
       navigationHelpButton: false,
@@ -262,7 +328,7 @@
       // terrainProviderViewModels
       // baseLayer
       // terrainProvider
-      terrain: Terrain.fromWorldTerrain(),
+      // terrain: Terrain.fromWorldTerrain(),
       // skyBox
       // skyAtmosphere
       // fullscreenElement
@@ -295,14 +361,14 @@
     // 顯示FTS視窗
     viewer.scene.debugShowFramesPerSecond = true;
     // 設定自動遮蔽低於地形高度的模型
-    viewer.scene.globe.depthTestAgainstTerrain = true;
+    viewer.scene.globe.depthTestAgainstTerrain = false;
     // 設定是否允許攝影機進入地下
     viewer.scene.screenSpaceCameraController.enableCollisionDetection = true;
 
     // cs_viewer.extend(viewerCesium3DTilesInspectorMixin);
     // const inspectorViewModel = cs_viewer.cesium3DTilesInspector.viewModel;
 
-    const imageryLayers = viewer.imageryLayers;
+    // const imageryLayers = viewer.imageryLayers;
     
     // camera 飛到指定地點 使用經緯度、橢球高
     // let camera;
@@ -319,24 +385,67 @@
     return viewer;
   } 
 
-// 載入底圖
-  function load3DTilestLayer(viewer){
-    console.log('load3DTilestLayer')
-    // console.log('out: ',viewer);
-    // Add Cesium OSM Buildings, a global 3D buildings layer.
-    createOsmBuildingsAsync().then((buildingTileset)=>{
-      // console.log('osm-b: ',buildingTileset);
-      buildingTileset.show = false;
-      viewer.scene.primitives.add(buildingTileset);   
+// 載入底圖(廢棄)
+  // let googleTerrainLayer;
+  // function load3DTilestLayer(viewer){
+  //   console.log('load3DTilestLayer')
+  //   // console.log('out: ',viewer);
+  //   // Add Cesium OSM Buildings, a global 3D buildings layer.
+  //   createOsmBuildingsAsync().then((buildingTileset)=>{
+  //     // console.log('osm-b: ',buildingTileset);
+  //     buildingTileset.show = false;
+  //     viewer.scene.primitives.add(buildingTileset);   
+  //   })
+  //   Cesium3DTileset.fromUrl("https://tile.googleapis.com/v1/3dtiles/root.json?key=" + google_token.value).then(tileset=>{
+  //     googleTerrainLayer = tileset;
+  //     viewer.imageryLayers.add(googleTerrainLayer);
+  //     // console.log('goole: ',tileset);
+  //     // tileset.show = true;
+  //     // viewer.scene.primitives.add(tileset); 
+  //   });
+  // }
+
+  function getIonResouse(viewer){
+    // 使用 Ion API 獲取資源清單
+    // 獲取 Ion 資產清單
+    fetch('https://api.cesium.com/v1/assets', {
+      method: 'GET',
+      headers: {
+          'Authorization': `Bearer ${Ion.defaultAccessToken}`
+      }
     })
-    Cesium3DTileset.fromUrl("https://tile.googleapis.com/v1/3dtiles/root.json?key=" + google_token.value).then(tileset=>{
-      // console.log('goole: ',tileset);
-      tileset.show = false;
-      viewer.scene.primitives.add(tileset); 
+    .then(response => response.json())
+    .then(data => {
+      // console.log('Ion assets:', data);
+      data.items.forEach(asset => {
+        // 根據資產類型載入
+        switch (asset.type.toUpperCase()) {
+          case 'IMAGERY':
+            addBaseLayerOption(asset.name, IonImageryProvider.fromAssetId(2));
+            console.log(`Loaded imagery asset: ${asset.name}`);
+            break;
+
+          case 'TERRAIN':
+            addTerrainLayerOption(asset.name, CesiumTerrainProvider.fromIonAssetId(asset.id));
+            console.log(`Loaded terrain asset: ${asset.name}`);
+            break;
+
+          case '3DTILES':
+            add3DTilesLayerOption(asset.name, asset.id, 1, false);
+            console.log(`Loaded 3D tileset asset: ${asset.name}`);
+            break;
+
+          default:
+            console.log(`Unsupported asset type: ${asset.type}`);
+        }
+        
+      });
+      addTerrainLayerOption('無',new EllipsoidTerrainProvider());
+    })
+    .catch(error => {
+      console.error('Error fetching Ion assets:', error);
     });
   }
-
-  
 // 頁面渲染完成後執行步驟
   onMounted( async function () {
     cs_viewer = await initCesiumView(cs_viewer,'');
@@ -344,13 +453,17 @@
     
     imageryLayers = cs_viewer.imageryLayers;
     setupLayers();
+    getIonResouse();
 
     // 將toolbar綁定至knockout
     const toolbar = document.getElementById("toolbar");
     knockout.applyBindings(viewModel, toolbar);
+    // 監聽selectedLayer變化
     knockout
       .getObservable(viewModel, "selectedLayer")
       .subscribe(function (baseLayer) {
+        // console.log('knockout: ',baseLayer);
+        // console.log('cs_viewer: ',cs_viewer.imageryLayers);
         // Handle changes to the drop-down base layer selector.
         let activeLayerIndex = 0;
         const numLayers = viewModel.layers.length;
@@ -360,6 +473,7 @@
             break;
           }
         }
+        // console.log('activeLayerIndex: ',activeLayerIndex);
         const activeLayer = viewModel.layers[activeLayerIndex];
         const show = activeLayer.show;
         const alpha = activeLayer.alpha;
@@ -370,6 +484,18 @@
         updateLayerList();
         
       });
+    // 監聽terrainLayer變化
+    knockout
+      .getObservable(viewModel, "terrainLayer")
+      .subscribe(function (terrain) {
+        cs_viewer.scene.setTerrain(
+          new Terrain(terrain),
+        );
+
+      });
+
+      console.log('cesiumImgLayers: ',imageryLayers);
+      // console.log('viewModel: ',viewModel);
   });
 
   // 切換顯示圖層管理工具
@@ -420,7 +546,7 @@
                 <td><input type="checkbox" data-bind="checked: show"></td>
                 <td>
                   <span data-bind="text: name, visible: !$parent.isSelectableLayer($data)"></span>
-                  <select data-bind="visible: $parent.isSelectableLayer($data), options: $parent.baseLayers, optionsText: 'name', value: $parent.selectedLayer"></select>
+                  <select data-bind="visible: $parent.isSelectableLayer($data), options: $parent.baseLayers, optionsText: 'name', value: $parent.selectedLayer"></select>                  
                 </td>
                 <td>
                   <input type="range" min="0" max="1" step="0.01" data-bind="value: alpha, valueUpdate: 'input'">
@@ -434,6 +560,15 @@
                   <button type="button" class="cesium-button" data-bind="click: function() { $parent.lower($data, $index()); }, visible: $parent.canLower($index())">
                     ▼
                   </button>
+                </td>
+              </tr>
+              
+            </tbody>
+            <tbody>
+              <tr>
+                <td>目前地形</td>
+                <td>
+                  <select data-bind="options: terrainLayers, optionsText: 'name', value: terrainLayer"></select>
                 </td>
               </tr>
             </tbody>
@@ -453,6 +588,7 @@
   align-items: center;
   color: white;
   position: absolute;
+  max-width: calc(100% - 3rem);
   max-height: calc(100% - 150px);
   top: 2rem;
   left: 0.5rem;
@@ -468,9 +604,17 @@
 #layerManager table tr>td:nth-child(2){
   padding-left: 1rem;
 }
+#layerManager table tr>td:nth-child(1){
+  text-align: right;
+}
+
 #layerManager table tr {
   transform: translateY(0);
   transition: transform 0.4s ease-out;
+}
+
+table tr>td:nth-child(1){
+  min-width: 5rem;
 }
 
 #layerManager table tr.up {
